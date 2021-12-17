66b611641070265c6b1b8c4d486b97aa0d8fb274
jchristopherj <66082531+jchristopherj@users.noreply.github.com>
2021-12-17 15:33:10 -0300

    Add files via upload

---
+++ Origem.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include "Util.cpp"
+#include "head.h"
+
+
+bool is_possible(std::vector<int>valor, int candidato) {
+	if (valor.size() == 0) {
+		return true;
+	}
+	else {
+		switch (valor.size())
+		{
+		case 1:
+				if (valor[0] != candidato) {
+					return false;
+				}
+				break;
+		case 2:
+			if (valor[0] >= candidato) {
+				return false;
+			}
+			break;
+		case 3:
+			if (valor[2] != candidato) {
+				return false;
+			}
+			break;
+		default:
+			break;
+		}
+		
+	}
+	return true;
+	
+}
+
+bool dominios(std::vector<std::vector<int>>&d, std::vector<int>&candidatos, int aux) {
+	
+	if (candidatos.size() == 4) {
+		printSequence(candidatos);
+		return true;
+	}
+	else {
+		
+		for (int i = 0; i < d[aux].size(); i++) {			
+				if (is_possible(candidatos, d[aux][i])) {
+					candidatos.push_back(d[aux][i]);
+					dominios(d, candidatos,aux++);
+					candidatos.pop_back();					
+				}
+		}
+	}
+	return false;
+}
+
+int main() {
+	std::vector<std::vector<int>>d= { {1,2,3}, {1,2,3}, {1,2,3,4}, {2,3,4} };
+	std::cout << "Entre com os valores para os dominios: \n";
+	std::vector<int>candidatos({});
+	std::vector<int>restri({});
+	dominios(d,candidatos,0);
+}
---
+++ head.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <iostream>
+#include <vector>
+bool dominios(std::vector<std::vector<int>>&, std::vector<int>&, int);
